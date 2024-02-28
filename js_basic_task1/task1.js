b27c2bf2d372faccb52ed61f48c65f6a63f40a6e
Alisa Ladyka <ladykaalisa@gmail.com>
2024-02-28 11:01:47 +0200

    task1/2

---
+++ js_basic_task1/task1.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const log = console.log;
+
+log(chalk.red('This text red!'));
+
+log(chalk.blue.bold('This text blue and bold!'));
+
+log(chalk.green('This text green!'));
+
+
---
+++ js_basic_task1/task2.js
@@ -0,0 +1,7 @@
+let isNumber = 10;
+
+let isString = "it`s string";
+
+let isBoolean = true;
+
+console.log(isNumber, isString, isBoolean);
