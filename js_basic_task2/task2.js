9bf3da9f10d232fcb54738d5c662bb172ccafd90
Alisa Ladyka <ladykaalisa@gmail.com>
2024-03-05 16:03:51 +0200

    fix task2

--- js_basic_task2/task2.js
+++ js_basic_task2/task2.js
@@ -1,10 +1,8 @@
-
-
-
-let basePriceCar = 10000;
-let yearOfIssue = 2020;
+const basePriceCar = 10000;
+let finalPriceCar = basePriceCar;
+let yearOfIssue = 2021;
 let hasClimateControl = true;
-let mileage = 52000;
+let mileage = 50500;
 let hasSafetyPackage = true;
 let isElectric = true;
 let percentYearOfIssue = 10;
@@ -12,38 +10,34 @@ let percentHasClimateControl = 7;
 let percentMileage = 5;
 let percentHasSafetyPackage = 8;
 let percentIsElectric = 15;
-let totalSum1 = 0;
-let totalSum2 = 0;
-let totalSum3 = 0;
-let totalSum4 = 0;
-let totalSum5 = 0;
 
-if (yearOfIssue >= 2019 && yearOfIssue <= 2022) {
-    totalSum1 = (basePriceCar * percentYearOfIssue) / 100;
-    console.log(`Base price was 10,000, production year increase +${percentYearOfIssue}% to the final price ${totalSum1}`);
+if (yearOfIssue>=2019 && yearOfIssue<=2022) {
+    finalPriceCar += (basePriceCar * percentYearOfIssue) / 100;
+    console.log(`Base price was 10,000, production year increase +${percentYearOfIssue}% to the final price ${finalPriceCar}`);
 }
+
 
 if (hasClimateControl) {
-    totalSum2 = (basePriceCar * percentHasClimateControl) / 100;
-    console.log(`Base price was 10,000, has climate control increase +${percentHasClimateControl}% to the final price ${totalSum2}`);
+    finalPriceCar += (basePriceCar * percentHasClimateControl)/100;
+    console.log(`Base price was 10,000, has climate control increase +${percentHasClimateControl}% to the final price ${finalPriceCar}`);
 }
 
-if (mileage >= 50000 && mileage <= 100000) {
-    totalSum3 = (basePriceCar * percentMileage) / 100;
-    console.log(`Base price was 10,000, mileage decrease -${percentMileage}% to the final price ${totalSum3}`);
+
+if (mileage>=50000 && mileage<=100000) {
+    finalPriceCar -= (basePriceCar * percentMileage)/100;
+    console.log(`Base price was 10,000, mileage decrease -${percentMileage}% to the final price ${finalPriceCar}`);
 }
 
 if (hasSafetyPackage) {
-    totalSum4 = (basePriceCar * percentHasSafetyPackage) / 100;
-    console.log(`Base price was 10,000, has safety package increase +${percentHasSafetyPackage}% to the final price ${totalSum4}`);
+    finalPriceCar += (basePriceCar * percentHasSafetyPackage)/100;
+    console.log(`Base price was 10,000, has safety package increase +${percentHasSafetyPackage}% to the final price ${finalPriceCar}`);
+
 }
 
 if (isElectric) {
-    totalSum5 = (basePriceCar * percentIsElectric) / 100;
-    console.log(`Base price was 10,000, is electric increase +${percentIsElectric}% to the final price ${totalSum5}`);
+    finalPriceCar += (basePriceCar *percentIsElectric)/100;
+    console.log(`Base price was 10,000, is electric increase +${percentIsElectric}% to the final price ${finalPriceCar}`);
 }
-
-let finalPriceCar = basePriceCar + totalSum1 + totalSum2 - totalSum3 + totalSum4 + totalSum5;
 
 console.log(`Final price auto: ${finalPriceCar}`);
 
