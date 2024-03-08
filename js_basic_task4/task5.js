4af559f8f7ac83b5a03d0bc07a815b0a4d210846
Alisa Ladyka <ladykaalisa@gmail.com>
2024-03-08 15:28:35 +0200

    add task5

---
+++ js_basic_task4/task5.js
@@ -0,0 +1,12 @@
+
+const numbers = [150, 23, 1, -53, 49];
+
+const areAllLessThan50 = numbers.every(number => number < 50);
+
+console.log("Чи всі елементи масиву менше 50:", areAllLessThan50);
+
+
+
+
+
+
---
+++ js_basic_task4/task6.js
@@ -0,0 +1,11 @@
+
+const names = ["John", "Fernando", "Alexandra", "Anatolii", "Arina", "Elon"];
+
+const resultName = names.find(name => name.length > 6);
+
+console.log("Перше ім'я з довжиною більше 6 символів:", resultName);
+
+
+
+
+
