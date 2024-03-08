24a03fea3e940a4c15bebda3ac840d8d078d68a8
Alisa Ladyka <ladykaalisa@gmail.com>
2024-03-08 15:16:22 +0200

    add task1

--- js_basic_task4/task1.js
+++ js_basic_task4/task1.js
@@ -1,5 +1,21 @@
-let inputString = "Тест заміна голосних літер";
+const numbers = [5, -4, 0, 6, 7, -1, 0, 0];
 
-let resultString = inputString.replace(/[аеиіоуюєїАЕИІОУЮЄЇ]/g, '*');
 
-console.log("Результат заміни голосних літер:", resultString);
+let positiveCount = 0;
+let negativeCount = 0;
+let zeroCount = 0;
+
+for (var i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    positiveCount++;
+  } else if (numbers[i] < 0) {
+    negativeCount++;
+  } else {
+    zeroCount++;
+  }
+}
+
+console.log("Positive numbers: " + positiveCount);
+console.log("Negative numbers: " + negativeCount);
+console.log("Zeros: " + zeroCount);
+
--- js_basic_task4/task3.js
+++ js_basic_task4/task3.js
@@ -1,16 +1,45 @@
-let number1 = -20;
-let number2 = 20;
+const basePriceCar = 10000;
+let finalPriceCar = basePriceCar;
+let yearOfIssue = 2021;
+let hasClimateControl = true;
+let mileage = 50500;
+let hasSafetyPackage = true;
+let isElectric = true;
+let percentYearOfIssue = 10;
+let percentHasClimateControl = 7;
+let percentMileage = 5;
+let percentHasSafetyPackage = 8;
+let percentIsElectric = 15;
 
-let minNumber = Math.min(number1, number2);
-let maxNumber = Math.max(number1, number2);
-
-console.log("Парні числа між", minNumber, "і", maxNumber, ":");
-
-for (let i = minNumber; i <= maxNumber; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
+if (yearOfIssue>=2019 && yearOfIssue<=2022) {
+    finalPriceCar += (basePriceCar * percentYearOfIssue) / 100;
+    console.log(`Base price was 10,000, production year increase +${percentYearOfIssue}% to the final price ${finalPriceCar}`);
 }
+
+
+if (hasClimateControl) {
+    finalPriceCar += (basePriceCar * percentHasClimateControl)/100;
+    console.log(`Base price was 10,000, has climate control increase +${percentHasClimateControl}% to the final price ${finalPriceCar}`);
+}
+
+
+if (mileage>=50000 && mileage<=100000) {
+    finalPriceCar -= (basePriceCar * percentMileage)/100;
+    console.log(`Base price was 10,000, mileage decrease -${percentMileage}% to the final price ${finalPriceCar}`);
+}
+
+if (hasSafetyPackage) {
+    finalPriceCar += (basePriceCar * percentHasSafetyPackage)/100;
+    console.log(`Base price was 10,000, has safety package increase +${percentHasSafetyPackage}% to the final price ${finalPriceCar}`);
+
+}
+
+if (isElectric) {
+    finalPriceCar += (basePriceCar *percentIsElectric)/100;
+    console.log(`Base price was 10,000, is electric increase +${percentIsElectric}% to the final price ${finalPriceCar}`);
+}
+
+console.log(`Final price auto: ${finalPriceCar}`);
 
 
 
