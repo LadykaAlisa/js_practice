54c1d8633eb1824a55bdeebe83d03078daf06b66
Ladyka Alisa <ladykaalisa@gmail.com>
2024-03-14 21:03:51 +0200

    task1 done

--- js_basic_task5/task1.js
+++ js_basic_task5/task1.js
@@ -1,10 +1,6 @@
-//let stringLenght = (string) => string.lenght;
-//let text = "Hello world!";
-//console.log("Кількість символів у рядку:", stringLenght(text));
+let stringLength = (string) => string.length;
+let text = "Hello, world!";
+console.log("Кількість символів у рядку:", stringLength(text));
 
 
-const lengthOfString = (string) => string.length;
 
-// Приклад використання:
-let inputString = "Це є тестовий рядок.";
-console.log("Довжина рядка:", lengthOfString(inputString));
