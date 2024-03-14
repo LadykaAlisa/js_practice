d8a4e1c270d090c7f741ef6968163a2321efb965
Ladyka Alisa <ladykaalisa@gmail.com>
2024-03-14 22:00:15 +0200

    task4 done

--- js_basic_task5/task4.js
+++ js_basic_task5/task4.js
@@ -1,9 +1,25 @@
+function calculatePerformance(math, literature, physics) {
+    let averageScore = (math + literature + physics) / 3;
 
-const names = ["John", "Fernando", "Alexandra", "Anatolii", "Arina", "Elon"];
+    if (averageScore >= 1 && averageScore <= 49) {
+        return "Low performance";
+    } else if (averageScore >= 50 && averageScore <= 69) {
+        return "Medium performance";
+    } else if (averageScore >= 70 && averageScore <= 89) {
+        return "Good performance";
+    } else if (averageScore >= 90 && averageScore <= 100) {
+        return "Very good performance";
+    } else {
+        return "Invalid input"; 
+    }
+}
 
-const resultName = names.find(name => name.length > 6);
 
-console.log("Перше ім'я з довжиною більше 6 символів:", resultName);
+let mathGrade = 100;
+let literatureGrade = 100;
+let physicsGrade = 100;
+console.log(calculatePerformance(mathGrade, literatureGrade, physicsGrade));
+
 
 
 
