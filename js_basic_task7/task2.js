572e2ed41a74f12e51c9e277935b6d1ff32bd0fb
Ladyka Alisa <ladykaalisa@gmail.com>
2024-03-20 22:02:45 +0200

    task2 done

--- js_basic_task7/task2.js
+++ js_basic_task7/task2.js
@@ -1,9 +1,23 @@
-function divider(divisor) {
-  return function(dividend) {
-      return dividend / divisor;
-  };
-}
+const person = new Object();
+person.name = "Alice";
+person.age = 25;
+person.gender = "female";
+person.greet = function() {
+    return `Hello, my name is ${this.name}.`;
+};
 
-const divideByFive = divider(5);
-console.log(divideByFive(10)); 
+const car = new Object();
+car.brand = "BMW";
+car.model = "X5";
+car.year = 2020;
+car.drive = function() {
+    return "The car is driving.";
+};
 
+
+console.log(person.name);
+
+console.log(car.brand);
+
+console.log(person.greet()); 
+console.log(car.drive()); 
