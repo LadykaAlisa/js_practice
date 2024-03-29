f358db6ea8efb16ed33abbaabf8666d380386ee8
Ladyka Alisa <ladykaalisa@gmail.com>
2024-03-29 09:37:27 +0200

    task2 done

--- js_basic_task9/task2.js
+++ js_basic_task9/task2.js
@@ -1,23 +1,10 @@
-const person = new Object();
-person.name = "Alice";
-person.age = 25;
-person.gender = "female";
-person.greet = function() {
-    return `Hello, my name is ${this.name}.`;
-};
+function executeCallbackWithDelay(text, seconds, callback) {
+    setTimeout(function() {
+        callback(text);
+    }, seconds * 1000); 
+}
 
-const car = new Object();
-car.brand = "BMW";
-car.model = "X5";
-car.year = 2020;
-car.drive = function() {
-    return "The car is driving.";
-};
-
-
-console.log(person.name);
-
-console.log(car.brand);
-
-console.log(person.greet()); 
-console.log(car.drive()); 
+executeCallbackWithDelay("Цей текст виведеться через 3 секунди", 3, function(text) {
+    console.log("Отриманий текст:", text);
+    
+});
